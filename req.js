3f397a3e5243a6fee4bd891d645364e85321822c
Pietro Mizael <pietromizael@gmail.com>
2024-01-24 12:47:27 -0300

    prod 2

--- req.js
+++ req.js
@@ -57,6 +57,6 @@ app.get("/api/users", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
 });
